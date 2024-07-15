87e9c4f41fd193b30f6eed97d29aec0a6b632e57
Rotimi Jude <excel.rotimi@gmail.com>
2024-07-15 11:16:51 -0400

    second commit

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,26 @@
+import { useState } from 'react'
 import './Create.css'
 
 export default function Create() {
+  const [title, setTitle]=useState('')
+  const [method, setMethod]=useState('')
+  const [cookingTime, setCookingTime]=useState('')
+
+
   return (
-    <div>Create</div>
+    <div className='create'>
+      <h2 className='page-title'>Add a New Recipe</h2>
+
+      <form>
+        <label>
+          <span>Recipe Title:</span>
+          <input
+          type='text'
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required/>
+        </label>
+      </form>
+    </div>
   )
 }
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,7 +1,37 @@
+import {useFetch} from "../../hooks/useFetch"
+import {useParams, useHistory} from "react-router-dom"
+import { useEffect } from "react"
 import './Recipe.css'
 
 export default function Recipe() {
+  const {id} = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const {data:recipes, isPending, error} = useFetch(url)
+  const history = useHistory()
+
+  useEffect( ()=> {
+    if (error){
+        // redirect
+        //history.goBack()
+        
+        setTimeout(() => {
+            history.push('/')
+        }, 2000)
+
+    }
+}, [error, history])
   return (
-    <div>Recipe</div>
+    <div className="recipe">
+      {isPending && <div className="loading">Loading...</div>}
+      {error && <div className="error">{error}</div>}
+      {recipes && (<> 
+      <h2 className="page-title">{recipes.title}</h2>
+      <p>Takes {recipes.cookingTime} to cook.</p>
+      <ul>
+        {recipes.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+      </ul>
+      <p className="method">{recipes.method}</p>
+      </>)}
+    </div>
   )
 }
