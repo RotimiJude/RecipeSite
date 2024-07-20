e6aa7549e47a100248a63256dadd8369d9399ea7
Rotimi Jude <excel.rotimi@gmail.com>
2024-07-20 08:17:21 -0400

    started using reducers and usereducer hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,14 @@ import './Navbar.css'
 import Searchbar from './Searchbar'
 import { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
+import { useTheme } from '../hooks/useTheme'
 
 export default function Navbar() {
-  const {color} = useContext(ThemeContext)
+  const {color, changeColor} = useTheme()
+  
   return (
     <div className='navbar' style={{background: color}}>
-        <nav>
+        <nav onClick={() => changeColor('pink')}>
             <Link to='/' className="brand">
             <h1>Cooking Recipe Site</h1>
             </Link>
